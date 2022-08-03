78eee07aa64a8f430d9fce7939e723062537c849
Ayush <ayushkumarjain1105@gmail.com>
2022-08-03 08:25:40 +0530

    first commit

---
+++ Spring-q2/src/main/java/com/example/Springq2/SpringQ2Application.java
@@ -0,0 +1,13 @@
+package com.example.Springq2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQ2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQ2Application.class, args);
+	}
+
+}
---
+++ Spring-q2/src/test/java/com/example/Springq2/SpringQ2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springq2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQ2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
